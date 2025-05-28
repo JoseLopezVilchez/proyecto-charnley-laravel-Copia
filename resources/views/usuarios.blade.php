f1a4457be242754ac775755ce4a9832a748daa48
Jose Lopez Vilchez <jose.lopez@escuelaestech.es>
2025-05-28 12:24:34 +0200

    scroll en listado de usuarios

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -1,8 +1,100 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <p class="text-black">Usuarios</p>
+    <div class="flex w-full h-full overflow-hidden">
+        <div class="flex flex-col w-1/2 h-full">
+
+        </div>
+        <div class="w-1/2 h-full overflow-y-auto">
+            <div class="w-full h-fit flex flex-wrap justify-around gap-y-4">
+                <!-- Inicio de placeholders -->
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="avatar">
+                        <div class="w-48 rounded-full">
+                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title">Nombre Apellido</h3>
+                        <sub class="font-light">correoelectronico@correo.com</sub>
+                    </div>
+                </div>
+                <!-- Fin de placeholders -->
             </div>
         </div>
     </div>
