777ea0c73e21304605c269c9a066fe9acbe6c2c6
Jose Lopez Vilchez <jose.lopez@escuelaestech.es>
2025-05-29 10:46:30 +0200

    sustitucion de avatares gordos por tabla - queda más limpio

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -17,31 +17,202 @@
             
         </div>
         <div class="w-1/2 h-full overflow-y-auto">
-            <div class="w-full h-fit flex flex-wrap justify-around gap-y-4">
-                <div class="card">
-                    <div class="avatar">
-                        <div class="w-48 rounded-full">
-                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
-                        </div>
-                    </div>
-                    <div class="card-body items-center">
-                        <h3 class="card-title text-center">Nuevo</h3>
-                    </div>
-                </div>
-                <!-- Inicio de placeholders -->
-                <div class="card">
-                    <div class="avatar">
-                        <div class="w-48 rounded-full">
-                            <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
-                        </div>
-                    </div>
-                    <div class="card-body items-center">
-                        <h3 class="card-title text-center">Nombre Apellido</h3>
-                        <sub class="font-light text-center">correoelectronico@correo.com</sub>
-                    </div>
-                </div>
-                <!-- Fin de placeholders -->
-            </div>
+            <table class="table table-zebra">
+                <thead class="sticky top-0 z-10 bg-base-300">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Edad</th>
+                        <th>Rol</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Inicio de placeholders -->
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <tr>
+                        <td class="flex items-center">
+                            <div class="avatar mr-2">
+                                <div class="w-8 rounded-full">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F550%2F731%2Foriginal%2Fuser-icon-vector.jpg&f=1&nofb=1&ipt=6ff6c60830f1e2a7a7f28bb7a7e0c31651307083ecb10339c44c2b8496ef8f7d" />
+                                </div>
+                            </div>
+                            <h3>Nombre Apellido</h3>
+                        </td>
+                        <td>333</td>
+                        <td>Administradoroalgoasi</td>
+                        <td>correoelectronico@correo.com</td>
+                    </tr>
+                    <!-- Fin de placeholders -->
+                </tbody>
+            </table>
         </div>
     </div>
 </x-app-layout>
