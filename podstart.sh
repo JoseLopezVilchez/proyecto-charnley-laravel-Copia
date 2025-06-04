247907ffe895d7edb814f00b23a2207e7dcf735d
Jose Lopez Vilchez <kaltivelelnigromante@gmail.com>
2025-06-04 18:55:12 +0200

    probando a ver si va el podman

--- podstart.sh
+++ podstart.sh
@@ -20,5 +20,11 @@ sed "s|/changeme|$REPO_PATH|g" "$TEMPLATE" > "$TMP_YAML"
 chmod -R a+rx "$REPO_PATH"
 chown -R 1000:1000 "$REPO_PATH"
 
+# Limpiar pod previo si existe
+if podman pod exists charnley; then
+    echo "[INFO] Eliminando pod anterior..."
+    podman pod rm -f charnley
+fi
+
 # Crear pod desde el YAML modificado
 podman play kube "$TMP_YAML"
